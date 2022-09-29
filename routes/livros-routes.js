556c3b2e261ec14a4c34171be2b7bb50f977f9cc
BrunaLima123 <bruna.lima@aluno.unc.br>
2022-09-28 22:21:32 -0300

    arrumando mensagens

--- routes/livros-routes.js
+++ routes/livros-routes.js
@@ -11,16 +11,14 @@ const {
 // inicializando as rotas do express
 const router = express.Router()
 
-// criando as rotas para o recurso 'livro'
-// definindo a rota para a listagem de livro
 router.get('/livro', getAllLivro)
-// definindo a rota para listar livro específico
+
 router.get('/livro/:id', getLivro)
-// definindo a rota para cadastro de livro
+
 router.post('/livro', addLivro)
-// definindo a rota para alterar um livro
+
 router.put('/livro/:id', updateLivro)
-// definindo a rota para excluir um livro
+
 router.delete('/livro/:id', deleteLivro)
 
 module.exports = {
